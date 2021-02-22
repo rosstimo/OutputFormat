f80708e7c1e9dd4f322a388a272d192b3d270bb8
rosstimo <rosstimo@isu.edu>
2021-02-22 08:52:19 -0700

    add a little formatting fun

--- OutputFormat/OutputFormatForm.vb
+++ OutputFormat/OutputFormatForm.vb
@@ -6,10 +6,13 @@ Public Class OutputFormatForm
     Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayButton.Click
 
         'Display multi-line formatted text
-        DisplayLabel.Text = "1. Display a line of text" & vbNewLine _
-                            & "2. Display a line of text" & vbNewLine _
-                            & "3. Display a line of text" & vbNewLine _
-                            & "4. Display a line of text"
+        'DisplayLabel.Text = "1. Display a line of text" & vbNewLine _
+        '                    & "2. Display a line of text" & vbNewLine _
+        '                    & "3. Display a line of text" & vbNewLine _
+        '                    & "4. Display a line of text"
+
+        DisplayLabel.Text = Trim(StrReverse(FirstNameTextBox.Text)) & " " & Trim(StrReverse(LastNameTextBox.Text))
+        AgeTextBox.Text = FirstNameTextBox.Text.ToLower & LastNameTextBox.Text.ToLower & "@acme.net"
 
     End Sub
 
