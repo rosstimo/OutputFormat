9d5ecd7513d8e060c4ee85fba847d8f0f3ce1526
rosstimo <rosstimo@isu.edu>
2021-02-23 08:47:10 -0700

    add field validation

--- OutputFormat/OutputFormatForm.Designer.vb
+++ OutputFormat/OutputFormatForm.Designer.vb
@@ -33,11 +33,16 @@ Partial Class OutputFormatForm
         Me.FirstNameLabel = New System.Windows.Forms.Label()
         Me.LastNameLabel = New System.Windows.Forms.Label()
         Me.AgeLabel = New System.Windows.Forms.Label()
+        Me.EmailLabel = New System.Windows.Forms.Label()
+        Me.EmailTextBox = New System.Windows.Forms.TextBox()
+        Me.ZipLabel = New System.Windows.Forms.Label()
+        Me.ZipTextBox = New System.Windows.Forms.TextBox()
+        Me.ClearButton = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'DisplayButton
         '
-        Me.DisplayButton.Location = New System.Drawing.Point(384, 346)
+        Me.DisplayButton.Location = New System.Drawing.Point(179, 346)
         Me.DisplayButton.Name = "DisplayButton"
         Me.DisplayButton.Size = New System.Drawing.Size(199, 92)
         Me.DisplayButton.TabIndex = 0
@@ -112,6 +117,48 @@ Partial Class OutputFormatForm
         Me.AgeLabel.TabIndex = 8
         Me.AgeLabel.Text = "Age"
         '
+        'EmailLabel
+        '
+        Me.EmailLabel.AutoSize = True
+        Me.EmailLabel.Location = New System.Drawing.Point(26, 127)
+        Me.EmailLabel.Name = "EmailLabel"
+        Me.EmailLabel.Size = New System.Drawing.Size(48, 20)
+        Me.EmailLabel.TabIndex = 10
+        Me.EmailLabel.Text = "Email"
+        '
+        'EmailTextBox
+        '
+        Me.EmailTextBox.Location = New System.Drawing.Point(89, 121)
+        Me.EmailTextBox.Name = "EmailTextBox"
+        Me.EmailTextBox.Size = New System.Drawing.Size(285, 26)
+        Me.EmailTextBox.TabIndex = 9
+        '
+        'ZipLabel
+        '
+        Me.ZipLabel.AutoSize = True
+        Me.ZipLabel.Location = New System.Drawing.Point(26, 159)
+        Me.ZipLabel.Name = "ZipLabel"
+        Me.ZipLabel.Size = New System.Drawing.Size(31, 20)
+        Me.ZipLabel.TabIndex = 12
+        Me.ZipLabel.Text = "Zip"
+        '
+        'ZipTextBox
+        '
+        Me.ZipTextBox.Location = New System.Drawing.Point(89, 153)
+        Me.ZipTextBox.Name = "ZipTextBox"
+        Me.ZipTextBox.Size = New System.Drawing.Size(285, 26)
+        Me.ZipTextBox.TabIndex = 11
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(384, 346)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(199, 92)
+        Me.ClearButton.TabIndex = 13
+        Me.ClearButton.Text = "&Clear"
+        Me.DisplayFormToolTip.SetToolTip(Me.ClearButton, "Click to clear all fields")
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
         'OutputFormatForm
         '
         Me.AcceptButton = Me.DisplayButton
@@ -119,6 +166,11 @@ Partial Class OutputFormatForm
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.ExitButton
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.ZipLabel)
+        Me.Controls.Add(Me.ZipTextBox)
+        Me.Controls.Add(Me.EmailLabel)
+        Me.Controls.Add(Me.EmailTextBox)
         Me.Controls.Add(Me.AgeLabel)
         Me.Controls.Add(Me.LastNameLabel)
         Me.Controls.Add(Me.FirstNameLabel)
@@ -145,4 +197,9 @@ Partial Class OutputFormatForm
     Friend WithEvents FirstNameLabel As Label
     Friend WithEvents LastNameLabel As Label
     Friend WithEvents AgeLabel As Label
+    Friend WithEvents EmailLabel As Label
+    Friend WithEvents EmailTextBox As TextBox
+    Friend WithEvents ZipLabel As Label
+    Friend WithEvents ZipTextBox As TextBox
+    Friend WithEvents ClearButton As Button
 End Class
--- OutputFormat/OutputFormatForm.vb
+++ OutputFormat/OutputFormatForm.vb
@@ -4,20 +4,66 @@ Option Explicit On
 Public Class OutputFormatForm
 
     Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayButton.Click
-
+        ValidateFeilds()
         'Display multi-line formatted text
         'DisplayLabel.Text = "1. Display a line of text" & vbNewLine _
         '                    & "2. Display a line of text" & vbNewLine _
         '                    & "3. Display a line of text" & vbNewLine _
         '                    & "4. Display a line of text"
 
-        DisplayLabel.Text = Trim(StrReverse(FirstNameTextBox.Text)) & " " & Trim(StrReverse(LastNameTextBox.Text))
-        AgeTextBox.Text = FirstNameTextBox.Text.ToLower & LastNameTextBox.Text.ToLower & "@acme.net"
+        'DisplayLabel.Text = Trim(StrReverse(FirstNameTextBox.Text)) & " " & Trim(StrReverse(LastNameTextBox.Text))
+        'AgeTextBox.Text = FirstNameTextBox.Text.ToLower & LastNameTextBox.Text.ToLower & "@acme.net"
 
     End Sub
 
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        FirstNameTextBox.Text = ""
+        LastNameTextBox.Text = ""
+        AgeTextBox.Text = ""
+        EmailTextBox.Text = ""
+        ZipTextBox.Text = ""
+        DisplayLabel.Text = ""
+    End Sub
+
+    Function ValidateFeilds() As Boolean
+        Dim problemMessage As String
+
+        'If AgeTextBox.Text = "" Then
+        'problemMessage &= "Age is required" & vbNewLine
+        'AgeTextBox.Focus()
+        'Else
+        Try
+            If CInt(AgeTextBox.Text) > 42 Then
+                problemMessage &= "You are too old!" & vbNewLine
+            End If
+        Catch ex As Exception
+            problemMessage &= "Age should be a number" & vbNewLine
+            AgeTextBox.Focus()
+        End Try
+        'End If
+
+        If LastNameTextBox.Text = "" Then
+            problemMessage &= "Last name is required" & vbNewLine
+            LastNameTextBox.Focus()
+        End If
+
+        If FirstNameTextBox.Text = "" Then
+            problemMessage &= "First name is required" & vbNewLine
+            FirstNameTextBox.Focus()
+        End If
+
+        If problemMessage <> "" Then
+            MsgBox(problemMessage)
+        End If
+
+
+
+        Return True
+    End Function
+
 
 End Class
